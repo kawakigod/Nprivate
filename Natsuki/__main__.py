1239bd0310d8a3c7a1b604cbdeb0eccba6961ff7
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-09-11 19:46:59 +0530

    Update __main__.py

--- Natsuki/__main__.py
+++ Natsuki/__main__.py
@@ -65,7 +65,7 @@ from Natsuki.modules.helper_funcs.readable_time import get_readable_time
 PM_START_TEXT = """
 👋 Hello There, I'm 𝗡𝗮𝘁𝘀𝘂𝗸𝗶. I'm a Powerfull Group Manager Bot With Cool Modules. @TeamNatsuki
 
-Guys. /help to Find My List Of Available Commands 🙋‍♂️
+Guys. /help to find My list Of Available Commands 🙋‍♂️
 
 """
 
