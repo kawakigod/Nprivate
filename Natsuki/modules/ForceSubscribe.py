4a0b31512246a90c5a572893f31593d72202ac08
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-09-22 19:06:33 +0530

    Update ForceSubscribe.py

--- Natsuki/modules/ForceSubscribe.py
+++ Natsuki/modules/ForceSubscribe.py
@@ -196,7 +196,7 @@ def config(client, message):
 __help__ = """
 *ForceSubscribe:*
 
-❂ Daisy can mute members who are not subscribed your channel until they subscribe
+❂ Natsuki can mute members who are not subscribed your channel until they subscribe
 ❂ When enabled I will mute unsubscribed members and show them a unmute button. When they pressed the button I will unmute them
 
 *Setup*
@@ -204,6 +204,7 @@ __help__ = """
 Note: Only creator of the group can setup me and i will not allow force subscribe again if not done so.
  
 *Commmands*
+
 ❂ /ForceSubscribe - To get the current settings.
 ❂ /ForceSubscribe no/off/disable - To turn of ForceSubscribe.
 ❂ /ForceSubscribe {channel username} - To turn on and setup the channel.
@@ -213,4 +214,4 @@ Note: /FSub is an alias of /ForceSubscribe
 
  
 """
-__mod_name__ = "Force Subscribe"
+__mod_name__ = "F-Sub"
